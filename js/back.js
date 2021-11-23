a225e1d0637b867e8b4291901dd7a6e2b0d1f18f
473557950@qq.com <473557950@qq.com>
2021-11-24 01:27:55 +0800

    优化back.js

--- js/back.js
+++ js/back.js
@@ -1,97 +1,35 @@
+var serverUrl = "http://duqiu.natapp1.cc/cookiecollection/cookie-servlet"
+var clearInterval = 1000*5
+
 // 监听来自contentScrip的通知
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
     if (request.domain == "iqiyi") {
         if (request.operation == "getCookie") {
-            var url = "http://duqiu.natapp1.cc/cookiecollection/cookie-servlet?domain=iqiyi";
-            let xhr = new XMLHttpRequest();
-            // 使用同步
-            xhr.open("GET",url,false);
-            xhr.onload = function () {
-                var responseText = xhr.responseText;
-                const arr =  JSON.parse(responseText);
-                sendResponse(arr);
-            }
-            xhr.onerror = function () {
-                alert("请求出错");
-            }
-            xhr.send();
+            getSendCookie("iqiyi",sendResponse)
         } else if (request.operation == "clearCookie") {
             chrome.cookies.getAll({domain:".iqiyi.com"},function(cks) {
-                for (const ck of cks) {
-                    var newCookie = {};
-                    newCookie.name=ck.name;
-                    newCookie.url="https://www.iqiyi.com/";
-                    newCookie.storeId=ck.storeId;
-                    chrome.cookies.remove(newCookie);
-
-                    newCookie.url = "https://" + ck.domain + ck.path;
-                    chrome.cookies.remove(newCookie);
-                }
+                clearCookies(cks)
             });
         }
     } else if (request.domain == "qq") {
         if (request.operation == "getCookie") {
-            console.log(request, sender, sendResponse);
-            let xhr = new XMLHttpRequest();
-            // 使用同步
-            xhr.open("GET","http://duqiu.natapp1.cc/cookiecollection/cookie-servlet?domain=qq",false);
-            xhr.onload = function () {
-                var responseText = xhr.responseText;
-                const arr =  JSON.parse(responseText);
-                sendResponse(arr);
-            }
-            xhr.onerror = function () {
-                alert("请求出错");
-            }
-            xhr.send();
+            getSendCookie("qq",sendResponse)
         } else if (request.operation == "clearCookie") {
             setTimeout(function () {
                 chrome.cookies.getAll({domain:".qq.com"},function(cks) {
-                    for (const ck of cks) {
-                        var newCookie = {};
-                        newCookie.name=ck.name;
-                        newCookie.storeId=ck.storeId;
-
-                        newCookie.url = "https://v.qq.com" + ck.path;
-                        chrome.cookies.remove(newCookie);
-
-                        newCookie.url = "https://video.qq.com" + ck.path;
-                        chrome.cookies.remove(newCookie);
-
-                        newCookie.url = "https://" + ck.domain + ck.path;
-                        chrome.cookies.remove(newCookie);
-                    }
+                    clearCookies(cks)
                 });
             },1000*3);
 
         }
     }else if (request.domain == "youku") {
         if (request.operation == "getCookie") {
-            console.log(request, sender, sendResponse);
-            let xhr = new XMLHttpRequest();
-            // 使用同步
-            xhr.open("GET","http://duqiu.natapp1.cc/cookiecollection/cookie-servlet?domain=youku",false);
-            xhr.onload = function () {
-                var responseText = xhr.responseText;
-                // console.log(responseText);
-                const arr =  JSON.parse(responseText);
-                sendResponse(arr);
-            }
-            xhr.onerror = function () {
-                alert("请求出错");
-            }
-            xhr.send();
+            getSendCookie("youku",sendResponse)
         } else if (request.operation == "clearCookie") {
             setTimeout(function () {
                 chrome.cookies.getAll({domain:".youku.com"},function(cks) {
-                    for (const ck of cks) {
-                        var newCookie = {};
-                        newCookie.name=ck.name;
-                        newCookie.storeId=ck.storeId;
-                        newCookie.url = "https://" + ck.domain + ck.path;
-                        chrome.cookies.remove(newCookie);
-                    }
+                    clearCookies(cks)
                 });
             },1000*3);
 
@@ -100,5 +38,37 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 
 });
 
+function clearCookies(cks) {
+    for (const ck of cks) {
+        var newCookie = {};
+        newCookie.name=ck.name;
+        newCookie.storeId=ck.storeId;
+        newCookie.url = "https://" + ck.domain + ck.path;
+        chrome.cookies.remove(newCookie);
+    }
+}
 
+function getSendCookie(domain,sendResponse) {
+    var url;
+    if (domain == "qq") {
+        url = serverUrl + "?domain=qq"
+    } else if (domain == "iqiyi") {
+        url = serverUrl + "?domain=iqiyi"
+    } else if (domain == "youku") {
+        url = serverUrl + "?domain=youku"
+    }
+
+    let xhr = new XMLHttpRequest();
+    // 使用同步
+    xhr.open("GET",url,false);
+    xhr.onload = function () {
+        var responseText = xhr.responseText;
+        const arr =  JSON.parse(responseText);
+        sendResponse(arr);
+    }
+    xhr.onerror = function () {
+        alert("请求出错");
+    }
+    xhr.send();
+}
 
